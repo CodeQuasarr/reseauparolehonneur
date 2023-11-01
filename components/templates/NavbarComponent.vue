c4477722d68914b847341fd45132da276d4e18ae
Samuel ANYARONKE <dsanyaronke@gmail.com>
2023-11-01 19:52:36 +0100

    bar de navigation

---
+++ components/templates/NavbarComponent.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {
+    FwbNavbar,
+    FwbNavbarCollapse,
+    FwbNavbarLink,
+    FwbNavbarLogo,
+} from 'flowbite-vue'
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <fwb-navbar>
+            <template #logo="{}">
+                <a class="flex items-center" href="#">
+                    <img src="~/assets/images/logos/logo.webp" alt="Logo" class="mr-3 h-6 sm:h-10">
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                        RESEAU PAROLE HONNEUR
+                    </span>
+                </a>
+            </template>
+            <template #default="{isShowMenu}">
+                <fwb-navbar-collapse :is-show-menu="isShowMenu">
+                    <fwb-navbar-link is-active link="#">
+                        Home
+                    </fwb-navbar-link>
+                    <fwb-navbar-link link="#">
+                        Services
+                    </fwb-navbar-link>
+                    <fwb-navbar-link link="#">
+                        Pricing
+                    </fwb-navbar-link>
+                    <fwb-navbar-link link="#">
+                        Contact
+                    </fwb-navbar-link>
+                </fwb-navbar-collapse>
+            </template>
+        </fwb-navbar>
+    </div>
+</template>
+
+<style scoped>
+nav {
+    @apply border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 ;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import NavbarComponent from "~/components/templates/NavbarComponent.vue";
+</script>
+
+<template>
+    <div>
+        <div >
+            <NavbarComponent />
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
