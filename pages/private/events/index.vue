ba39b0064ec703a7a3b7fe39ccb192df155e709d
Samuel ANYARONKE <dsanyaronke@gmail.com>
2023-12-25 13:31:52 +0100

    delete event

--- pages/private/events/index.vue
+++ pages/private/events/index.vue
@@ -171,28 +171,28 @@ const handleInputChange = (event: any) => {
                             </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                            <tr v-for="event in events" :key="event.id" class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                            <tr v-for="evenement in events" :key="evenement.id" class="hover:bg-gray-100 dark:hover:bg-gray-700">
                                 <td class="w-4 p-4">
                                     <div class="flex items-center">
-                                        <input id="checkbox-{{ event.id }}" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-indigo-300 dark:focus:ring-indigo-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
-                                        <label for="checkbox-{{ event.id }}" class="sr-only">checkbox</label>
+                                        <input id="checkbox-{{ evenement.id }}" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-indigo-300 dark:focus:ring-indigo-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
+                                        <label for="checkbox-{{ evenement.id }}" class="sr-only">checkbox</label>
 
                                     </div>
                                 </td>
                                 <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                    <img class="w-10 h-10 rounded-full" :src="`${appUrl}/_nuxt/assets/images/events/${event.picture}`" :alt="event.picture">
+                                    <img class="w-10 h-10 rounded-full" :src="`${appUrl}/_nuxt/assets/images/events/${evenement.picture}`" :alt="evenement.picture">
                                     <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
-                                        <div class="text-base font-semibold text-gray-900 dark:text-white">{{ event.title }}</div>
+                                        <div class="text-base font-semibold text-gray-900 dark:text-white">{{ evenement.title }}</div>
                                     </div>
                                 </td>
-                                <td class="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">{{ convertDateToString(event.startDate) }}</td>
-                                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ event.startTime }}</td>
+                                <td class="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">{{ convertDateToString(evenement.startDate) }}</td>
+                                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ evenement.startTime }}</td>
                                 <td class="p-4 space-x-2 whitespace-nowrap">
-                                    <NuxtLink :to="`/private/events/${event.id}/edit`" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
+                                    <NuxtLink :to="`/private/events/${evenement.id}/edit`" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800">
                                         <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
                                         Edit event
                                     </NuxtLink>
-                                    <button @click="currentEventId = <string>event.id" type="button" data-modal-target="delete-event-modal" data-modal-toggle="delete-event-modal" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                                    <button @click="currentEventId = <string>evenement.id" type="button" data-modal-target="delete-event-modal" data-modal-toggle="delete-event-modal" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                         <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
                                         Delete event
                                     </button>
