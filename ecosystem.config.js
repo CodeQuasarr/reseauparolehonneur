a4b11be5d4aa6fee55a9c0ec1fd97e496d2c24fd
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-01-01 12:52:29 +0100

    eco malformated correction

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'my-nuxt-app-rph',
+            name: 'reseauparolehonneur',
             exec_mode: 'cluster',
             instances: 'max',
             script: './.output/server/index.mjs'
