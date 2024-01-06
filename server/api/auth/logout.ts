7696131318e10e5b6d2841dd1a8bf084d7daf36f
Samuel ANYARONKE <dsanyaronke@gmail.com>
2024-01-06 16:23:45 +0100

    correction sur la déconnection: supprimer le cookie evenement

--- server/api/auth/logout.ts
+++ server/api/auth/logout.ts
@@ -3,6 +3,7 @@ export default defineEventHandler(async (event) => {
 
     deleteCookie(event, 'authToken');
     deleteCookie(event, 'user')
+    deleteCookie(event, 'evenement')
 
     return 'successfully logged out'
 })
